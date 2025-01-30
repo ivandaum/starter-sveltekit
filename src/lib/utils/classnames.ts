1f2996d39f647b848cf1663bfde84facf9550b32
Ivan Daum <ivan.daum@gmail.com>
2025-01-30 22:16:31 +0100

    comment

--- src/lib/utils/classnames.ts
+++ src/lib/utils/classnames.ts
@@ -5,9 +5,8 @@ import { isString, isBool } from '$lib/utils/test';
 // prop is string -> show prop or concat
 
 /**
- * Return the chosen className if prop exists
- * Else return fallback if exists
- * Else return prop itself
+ * If prop is boolean and true, return fallback if exists (else return className)
+ * If prop is string, return className (assuming className is built with prop concat)
  */
 export const classNames = (prop: any, className: string | null = null, fallback: string = '') => {
 	if (isBool(prop) && prop) {
